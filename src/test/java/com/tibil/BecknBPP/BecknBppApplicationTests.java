c5b0687a365abd80a5222b8c6580d7cc7d665348
Sanjay-krishnan <sanjay.krishnan@tibilsolutions.com>
2023-03-06 16:58:52 +0530

    Initial commit - Added spring boot structure

---
+++ src/test/java/com/tibil/BecknBPP/BecknBppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibil.BecknBPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecknBppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
